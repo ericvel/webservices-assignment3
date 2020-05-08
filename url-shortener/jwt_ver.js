3864e01cf2dab393c06afea9977b2f727dfc9e0a
Eric Veliyulin <Eric@10.22.219.57>
2020-05-08 17:19:33 +0200

    Change localhost to ip

--- url-shortener/jwt_ver.js
+++ url-shortener/jwt_ver.js
@@ -7,7 +7,7 @@ module.exports =
     Verification: function (header_value)
     {
         return new Promise ((resolve, reject) => {
-            http.get('http://localhost:2000/user/token', async (res) => {
+            http.get('http://0.0.0.0:2000/user/token', async (res) => {
                 if (res.statusCode == 200) {
                     // Get JWT from response header
                     const loginToken = res.headers['x-access-token'];
